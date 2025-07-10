6bd3df665b97382374becf88e2f31827cd5b09aa
Cherrling <cherrling2021@outlook.com>
2025-07-10 22:04:12 +0800

    init

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -4,57 +4,42 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
 
 public class ExampleModClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-        KeyInputHandler.register();
-    }
-}
+	private static KeyBinding teleportKey;
+    private static boolean wasPressed = false;
 
-class KeyInputHandler {
-    private static KeyBinding sendKey;
-    private static KeyBinding sendJump;
-    private static int tickCountdown = -1;
-    private static boolean isFlagged = false;
-private static int ticksLeft = 0;
-private static int spamCount = 0;
-    private static double startPos = 0.0;
-    public static void register() {
-        sendKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.examplemod.send",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_Z,
-            "category.examplemod"
-        ));
-                sendJump = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.examplemod.jump",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_R,
-            "category.examplemod"
-        ));
+	@Override
+	public void onInitializeClient() {
+		teleportKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.teleportmod.teleport",
+			GLFW.GLFW_KEY_Z,
+			"category.teleportmod.main"
+		));
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player == null) return;
-            ClientPlayerEntity player = client.player;
-            if (sendKey.wasPressed()) {
-                player.setPosition(player.getX()+0.1, player.getY() + 0.2, player.getZ()+0.1);
-            }
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (teleportKey.isPressed() && !wasPressed) {
+				wasPressed = true;
+				teleportUp();
+			} else if (!teleportKey.isPressed()) {
+				wasPressed = false;
+			}
+		});
+	}
+
+	private void teleportUp() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player == null) return;
+
+        Vec3d currentPos = client.player.getPos();
+        Vec3d newPos = currentPos.add(0, 0.3, 0);
+        
+        client.player.setPosition(newPos);
             
-            if (sendJump.wasPressed()) {
-                player.setPosition(player.getX(), player.getY() + 1, player.getZ());
-            }
-
-
-            if (!isFlagged && player.getY() > -56.7 && player.getBlockY() > -59) {
-                player.networkHandler.sendChatMessage("Y = " + player.getY() + ", BlockY = " + player.getBlockY());
-player.networkHandler.sendChatCommand("flag");
-                isFlagged = true;
-            }
-
-        });
+        client.player.networkHandler.sendChatCommand("flag");
     }
 }
